4b5f9129948454b7b4c45d33c051771eb26fce3e
Usmonov12345 <toxirusmonov04@gmail.com>
2022-09-14 16:21:48 +0500

    3-dars

---
+++ main.js
@@ -0,0 +1,71 @@
+// const person = {
+//     name: "Jamoliddin"
+//     gender: "male"
+// }
+
+
+// function getName(person){
+//     person.getName
+// }
+
+// getName(person);
+
+// var jsStudents = ["1","2","2"];
+
+// var smmStudents = [];
+
+
+// var maxStudents = 5;
+
+
+// function addStudents(student){
+//     if(student.includes("JS")){
+//         jsStudents.push(student)
+//     }
+// }
+
+// if(jsStudents.length == 3){
+//     console.log("toldi");
+// }
+
+// addStudents("Toxir JS")
+
+
+// var studentd = [];
+// var qqstudentd = [];
+// var qstudentd = [];
+
+// function addstudents (student){
+//     if(student.includes("JS")){
+//         student.push(student)
+//     }
+//     if(student.length == 3){
+//         console.log("salom");
+//     }
+// }
+
+// addstudents("toxir js")
+
+// const phone = "iphone14ProMax"
+
+// switch (phone){
+//     case "Animal":
+//     console.log("not buy");
+//     break
+//     case"iphone14ProMax":
+//     console.log("olin");
+//     break
+//     case"alik":
+//     console.log("olasan olma");
+
+//     default : 
+//     console.log("ozin bilasan");
+
+// }
+
+
+const age = 12;
+
+const canSwim = age >= 14 ? "SWim" : age > 20 ? "can run" : "you cannot swim"
+
+console.log(canSwim);
